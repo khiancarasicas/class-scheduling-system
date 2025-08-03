8ecc537d8d0ae7099dbdfdae4bff7e6a24607a79
khiancarasicas <khianreendy.c@gmail.com>
2025-08-03 18:42:42 +0800

    add: department.ts - mock data

---
+++ lib/mock/departments.ts
@@ -0,0 +1,6 @@
+import { IDepartment } from "@/types/department";
+
+export const mockDepartments: IDepartment[] = [
+  { _id: "1", name: "Hospitality Management" },
+  { _id: "2", name: "Information Technology" },
+];
