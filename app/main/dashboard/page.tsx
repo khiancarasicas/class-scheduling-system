2e2d0c0fdc0ae55bb9047a08e7fb5ca40aa41896
khiancarasicas <khianreendy.c@gmail.com>
2025-08-16 20:48:02 +0800

    refactor: dashboard ui

--- app/main/dashboard/page.tsx
+++ app/main/dashboard/page.tsx
@@ -11,12 +11,12 @@ import {
 export default function Dashboard() {
   return (
     <main className="flex flex-1 flex-col">
-      <div className="text-base">Dashboard</div>
+      <div className="text-base font-medium">Dashboard</div>
       <div className="mt-2 grid gap-2 sm:grid-cols-2 lg:grid-cols-4">
         <InfoCardWrapper />
       </div>
       <Separator className="my-6" />
-      <div className="text-base">Today's Class Schedule</div>
+      <div className="text-base font-medium">Today's Class Schedule</div>
       <Card className="mt-2 h-600">
         <CardHeader>
           <CardDescription>Heh</CardDescription>
