0789376535ccd721f34083a488259ee648aaf619
khiancarasicas <khianreendy.c@gmail.com>
2025-08-24 22:18:14 +0800

    refactor: store - schedule of instructor, typo

--- lib/data-store/scheduleOfInstructorStore.ts
+++ lib/data-store/scheduleOfInstructorStore.ts
@@ -3,42 +3,42 @@ import { IScheduleOfInstructor } from "@/types";
 const scheduledOfInstructors: IScheduleOfInstructor[] = [
   {
     _id: "1",
-    assginedSubjectId: "1",
+    assignedSubjectId: "1",
     instructorId: "5",
   },
   {
     _id: "2",
-    assginedSubjectId: "2",
+    assignedSubjectId: "2",
     instructorId: "3",
   },
   {
     _id: "3",
-    assginedSubjectId: "3",
+    assignedSubjectId: "3",
     instructorId: "1",
   },
   {
     _id: "4",
-    assginedSubjectId: "4",
+    assignedSubjectId: "4",
     instructorId: "12",
   },
   {
     _id: "5",
-    assginedSubjectId: "5",
+    assignedSubjectId: "5",
     instructorId: "10",
   },
   {
     _id: "6",
-    assginedSubjectId: "6",
+    assignedSubjectId: "6",
     instructorId: "4",
   },
   {
     _id: "7",
-    assginedSubjectId: "7",
+    assignedSubjectId: "7",
     instructorId: "2",
   },
   {
     _id: "8",
-    assginedSubjectId: "8",
+    assignedSubjectId: "8",
     instructorId: "6",
   },
 ];
