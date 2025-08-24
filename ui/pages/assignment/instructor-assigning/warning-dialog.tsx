d7c23666f4586d1f372c0a58d54196a4c74c1646
khiancarasicas <khianreendy.c@gmail.com>
2025-08-24 23:02:23 +0800

    refactor: warning dialog

--- ui/pages/assignment/instructor-assigning/warning-dialog.tsx
+++ ui/pages/assignment/instructor-assigning/warning-dialog.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import * as React from "react";
-import { Dialog, DialogContent } from "@/shadcn/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+} from "@/shadcn/components/ui/dialog";
 import Image from "next/image";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 export function WarningDialog() {
   const [open, setOpen] = React.useState(true);
@@ -10,20 +15,18 @@ export function WarningDialog() {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="sm:max-w-md text-center">
-        <div className="flex flex-col items-center space-y-4">
-          <h2 className="text-lg font-semibold">
-            This page is not working properly yet
-          </h2>
+        <DialogHeader>
+          <DialogTitle>This page is not working properly yet</DialogTitle>
           <p className="text-sm text-muted-foreground">
             We are still working on it. Check back soon.
           </p>
-        </div>
+        </DialogHeader>
         <Image
-            src="/images/oki_lang.png" // replace with your image path
-            alt="Under construction"
-            width={1000}
-            height={120}
-          />
+          src="/images/oki_lang.png" // replace with your image path
+          alt="Under construction"
+          width={1000}
+          height={120}
+        />
       </DialogContent>
     </Dialog>
   );
