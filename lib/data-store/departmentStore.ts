bef9488d8671fdf9b20bb443fb9bd5a8c3fc0222
khiancarasicas <khianreendy.c@gmail.com>
2025-08-24 10:10:05 +0800

    edit: department store

--- lib/data-store/departmentStore.ts
+++ lib/data-store/departmentStore.ts
@@ -1,8 +1,8 @@
 import { IDepartment } from "@/types/index";
 
 const departments: IDepartment[] = [
-  { _id: "1", name: "Hospitality Management", code: "HM" },
-  { _id: "2", name: "Information Technology", code: "IT" },
+  { _id: "1", name: "How Much", code: "HM" },
+  { _id: "2", name: "International Tambay", code: "IT" },
   { _id: "3", name: "Engineering", code: "ENGR" },
   { _id: "4", name: "Business and Management", code: "BM" },
   { _id: "5", name: "English", code: "ENG" },
